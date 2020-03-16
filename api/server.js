e83c2006afe71ab269ad2fddceb6d6f808e6fa89
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-03-16 12:57:14 -0700

    did first 2 stretch problems may go back and do the 3rd later

--- api/server.js
+++ api/server.js
@@ -1,9 +1,76 @@
 const express = require("express");
+const server = express();
 
 const db = require("../data/dbConfig.js");
 
-const server = express();
-
 server.use(express.json());
+
+
+
+
+
+server.get('/', (req, res) => {
+    res.send('Server is working')
+})
+
+server.get('/api/accounts', (req, res) => {
+    db.select('*').from('accounts')
+    .then(accounts => {
+        res.status(200).json({ accounts: accounts })
+    })
+    .catch(err => console.log(err))
+})
+
+server.get(`/api/accounts/:id`, (req, res) => {
+    db.select('*').from('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => console.log(err))
+})
+
+server.post('/api/accounts', (req, res) => {
+    db.select('*').from('accounts')
+    .insert(req.body, "id")
+    .then( item => {
+        if (item){
+            res.status(201).json({ new_post_id: item })
+        } else {
+            res.status(500).json({ error: "error adding account" })
+        }
+    })
+    .catch(err => console.log(err))
+})
+
+server.put(`/api/accounts/:id`, (req, res) => {
+    db.select('*').from('accounts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(item => {
+        if (item > 0){
+            res.status(200).json({ message: "updated successfully" })
+        } else {
+            res.status(404).json({ error: "404 error not found" })
+        }
+    })
+    .catch(err => console.log(err))
+})
+
+server.delete(`/api/accounts/:id`, (req, res) => {
+    db.select('*').from('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(item => {
+        if (item > 0){
+            res.status(200).json({ message: "item deleted successfully" })
+        } else {
+            res.status(404).json({ message: "404 error not found" })
+        }
+    })
+    .catch(err => console.log(err))
+})
+
 
 module.exports = server;
